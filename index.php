322758fabcbfead4cd59b7fd855aa2eeeb061ae2
Mukesh Chapagain <mukesh.chapagain@gmail.com>
2015-03-17 20:41:46 +0545

    Add confirmation popup when Delete link is clicked

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $result = mysql_query("SELECT * FROM users ORDER BY id DESC");
 		echo "<td>".$res['name']."</td>";
 		echo "<td>".$res['age']."</td>";
 		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\">Delete</a></td>";		
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
 	}
 	?>
 	</table>
