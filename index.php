1effe2115fc9eba5b9c8220f51183f9dd25720c4
Dhaval Prajapati <42002820+dhavz@users.noreply.github.com>
2020-10-02 15:31:30 +0530

    Protection against Cross Site Scripting

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
 	while($res = mysqli_fetch_array($result)) { 		
 		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
+		echo "<td>".htmlspecialchars($res['name'], ENT_QUOTES)."</td>";
+		echo "<td>".htmlspecialchars($res['age'], ENT_QUOTES)."</td>";
+		echo "<td>".htmlspecialchars($res['email'], ENT_QUOTES)."</td>";	
 		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
 	}
 	?>
